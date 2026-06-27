3f3fe48de56933cf5767c9f7a7ec924fbf8a75b1
Sergey-747 <serfre13@gmail.com>
2026-06-27 11:33:03 +0700

    Update smartphone.py

--- 03_lesson/smartphone.py
+++ 03_lesson/smartphone.py
@@ -3,5 +3,5 @@ class Smartphone:
     def __init__(self, brand, model, number):
         self.model = model
         self.brand = brand
-        self.namber = number
+        self.number = number
    
