d86df0090b207df6935d9a0959d58756274ab10f
Sergey-747 <serfre13@gmail.com>
2026-06-27 11:30:24 +0700

    Файлы перенесены в папку 03_lesson

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,14 @@
+from user import User
+
+boris = User('Борис', 'Гладковский')
+my_user = boris
+
+<<<<<<< HEAD
+my_user.F_name()
+my_user.L_name()
+my_user.UserName()
+=======
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
+>>>>>>> 955e0b14f1cdc9d7b80e22d945d10332e0ae3a16
---
+++ 03_lesson/lesson_3_task_2.py
@@ -0,0 +1,21 @@
+from smartphone import Smartphone
+
+catalog = [
+    Smartphone("Xiaomi", "Xiaomi 15 Pro", "+79132334567" ), 
+    Smartphone("Asus", "Asus ROG Phone 9 Pro", "+79912345678"),
+    Smartphone("Appole", "Appole 12", "+79132334567" ), 
+    Smartphone("Xiaomi", "Xiaomi Redmi 10C", "+79138544567"),
+    Smartphone("OPPO", "OPPO Find X9 Pro", "+79912348756"),
+    Smartphone("CMF Phone", "CMF Phone 2 Pro", "+79036812345")
+]
+<<<<<<< HEAD
+
+for infa in catalog:
+    print(f"{infa.brand} - {infa.model}. {infa.namber} ")
+
+    
+   
+=======
+for infa in catalog:
+    print(f"{infa.brand} - {infa.model}. {infa.number}")
+>>>>>>> 955e0b14f1cdc9d7b80e22d945d10332e0ae3a16
